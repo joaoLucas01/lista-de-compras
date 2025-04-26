235c5759a67b97475c9e1d11a3fa667b2c3bef39
joaolucas01 <jlmota0412@gmail.com>
2025-04-26 20:32:36 -0300

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+var inputitem = document.getElementById('input-item');
+var checkbox = document.getElementById('checkbox-1');
+function adicionar(){
+    console.log(item.value)
+    
+}
+
+function cortar(){
+
+}
